9451e2debaff2b727627037cfea9d6be7689c218
Vibhor <72573887+DevVibhor@users.noreply.github.com>
2023-11-09 22:02:02 +0530

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from "react-native";
-import SimpleBulletGraph from "./src/screens/SimpleBulletGraph/SimpleBulletGraph";
-
-AppRegistry.registerComponent(appName, () => SimpleBulletGraph);
