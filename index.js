0e26d4f982f6d83b384bcdfb90a642457cd0fed4
Vibhor <72573887+DevVibhor@users.noreply.github.com>
2023-11-09 21:44:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from "react-native";
+import SimpleBulletGraph from "./src/screens/SimpleBulletGraph/SimpleBulletGraph";
+
+AppRegistry.registerComponent(appName, () => SimpleBulletGraph);
